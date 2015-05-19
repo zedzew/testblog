37eee595e2af9a4abfc2ed3a6b9784c2236addd9
Igor Volyk <voluk.igor@gmail.com>
2015-05-20 01:13:57 +0300

    first commit

---
+++ firstdjango/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'firstdjango.views.home', name='home'),
+    url(r'', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+
+]
